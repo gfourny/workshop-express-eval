419a83e4b248724083b6de9d6f16a9f3fd786016
rapas44 <guillaume.fourny@ynov.com>
2018-01-26 15:53:13 +0100

    Exercise 5

--- src/app.js
+++ src/app.js
@@ -7,13 +7,19 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/home', function(req, res){
     res.end("express");
-})
+});
 
 app.get('/bingo', function(req, res){
-   // res.end("Bingo");
     var file = fs.readFileSync("numbers.txt");
     var content = file.toString().split(" ").join(', ');
-    res.end(content);
-})
+    var numbers = req.param('mynumbers');
+    numbers = numbers.split(',').join(', ');
+    if(numbers == content){
+        res.end('Bingo')
+    } else {
+        res.end("The bingo game is already started, sorry your numbers doesn't match with known numbers 31, 10, 80, 44, 66 ; so you can not say Bingo")
+    }
+    res.send('The bingo game is already started and known numbers are 31, 10, 80, 44, 66');
+});
 
 module.exports = app;
