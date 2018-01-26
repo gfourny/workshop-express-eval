f14f04b123111af7f742b20ebe318fcc9d4c521a
rapas44 <guillaume.fourny@ynov.com>
2018-01-26 16:21:26 +0100

    Exercise 5

--- src/app.js
+++ src/app.js
@@ -17,13 +17,17 @@ app.get('/home', function(req, res){
 app.get('/bingo', function(req, res){
     
     var numbers = req.param('mynumbers');
+    if(numbers == undefined) {
+        res.send('The bingo game is already started and known numbers are 31, 10, 80, 44, 66');
+    }
+    
     numbers = numbers.split(',').join(', ');
+    
     if(numbers == readFileNumbers()){
         res.end('Bingo')
     } else {
         res.end("The bingo game is already started, sorry your numbers doesn't match with known numbers 31, 10, 80, 44, 66 ; so you can not say Bingo")
     }
-    res.send('The bingo game is already started and known numbers are 31, 10, 80, 44, 66');
 });
 
 module.exports = app;
