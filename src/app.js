3abe8a7b12336a7d554a1e51b529dcbd851cbd77
rapas44 <guillaume.fourny@ynov.com>
2018-01-26 15:30:42 +0100

    Exercise 4

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 var express = require('express');
 const app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/home', function(req, res){
     res.end("express");
 })
 
 app.get('/bingo', function(req, res){
-    res.end("Bingo");
+   // res.end("Bingo");
+    var file = fs.readFileSync("numbers.txt");
+    var content = file.toString().split(" ").join(', ');
+    res.end(content);
 })
 
 module.exports = app;
