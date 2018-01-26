c94597db68e9650e863d4eb7b4a99674d40383ca
rapas44 <guillaume.fourny@ynov.com>
2018-01-26 15:04:46 +0100

    Exercise 2

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ let chalk = require('chalk');
 const app = require('./src/app');
 
 // port
-const port = 3000;
+const port = process.argv[2];
 
 // start server here
+app.listen(port);
 console.log(chalk.green('Hello web server'));
